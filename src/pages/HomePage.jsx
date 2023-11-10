b88b43af554a1cdb01b204661bfba0ea903a8389
Karen Schick <karen.schick6@gmail.com>
2023-11-10 11:31:01 -0500

    building home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { Container, Col, Image, Row } from 'react-bootstrap'
-import fall2023Image from "../img/fall2023.jpg"
+import React from "react";
+import { Container, Col, Image, Row } from "react-bootstrap";
+import fall2023Image from "../img/fall2023.jpg";
+import "../app.css";
 
 const HomePage = () => {
   return (
     <>
-    <div>HomePage</div>
-    <Container>
-      <Row>
-        <Col xs={6} md={4}>
-          <Image height="200px"src={fall2023Image} rounded />
+      <Container className="container">
+        <div className="homePage">
+          <Col className="homePageContent">
+            <h1 className="title">
+              <span>Hi!</span> <br />
+              <span>I'm Karen,</span><br />
+              <span>web developer.</span>
+            </h1>
+            <p className="homePageDescription">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              <br /> Dolorum, quas. Amet soluta assumenda cum?
+            </p>
+          </Col>
+        </div>
+        <Col className="homePageImage">
+          <Image src={fall2023Image} alt="HomePage Photo" />
         </Col>
-        </Row>
-    </Container>
+      </Container>
     </>
-  )
-}
-
-export default HomePage
+  );
+};
+export default HomePage;
