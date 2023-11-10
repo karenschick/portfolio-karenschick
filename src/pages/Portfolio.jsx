a86b58af5718fa1f309f438b66f787071c6e85d1
Karen Schick <karen.schick6@gmail.com>
2023-11-10 11:26:29 -0500

    portfolios display

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { LinkedInFollow } from "../components";
 import GitHubFollow from "../components/GitHubFollow/GitHubFollow";
+import { Card, Container, Row } from "react-bootstrap";
+
 const Portfolio = () => {
+  const [portfolios, setPortfolios] = useState([
+    {
+      id: 1,
+      title: "Project One",
+      description: "Project One description",
+      img: "",
+      link: "",
+    },
+    {
+      id: 2,
+      title: "Project Two",
+      description: "Project Two description",
+      img: "",
+      link: "",
+    },
+    {
+      id: 3,
+      title: "Project Three",
+      description: "Project Three description",
+      img: "",
+      link: "",
+    },
+  ]);
+
   return (
-    <div>
-      insert examples
-      <LinkedInFollow />
-      <GitHubFollow/>
-    </div>
+    <>
+      <div>
+        {portfolios.map((portfolio) => (
+          <Card key={portfolio.id} className="portfolio-card">
+            <h3>{portfolio.title}</h3>
+            <p>{portfolio.description}</p>
+          </Card>
+        ))}
+      </div>
+      <div>
+        <LinkedInFollow />
+        <GitHubFollow />
+      </div>
+    </>
   );
 };
 
---
+++ src/portfolioDB.js
@@ -0,0 +1,23 @@
+const portfolios = [
+    {
+        id: 1,
+        title: "Project One",
+        description: "Project One description",
+        img: "",
+        link: "",
+    },
+    {
+        id: 2,
+        title: "Project Two",
+        description: "Project One description",
+        img: "",
+        link: "",
+    },
+    {
+        id: 3,
+        title: "Project Three",
+        description: "Project One description",
+        img: "",
+        link: "",
+    },
+]
