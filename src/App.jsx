429bfada4f2ec97b528e34da76e030c234df28b2
Karen Schick <karen.schick6@gmail.com>
2023-11-10 19:50:28 -0500

    back to simple

--- src/App.jsx
+++ src/App.jsx
@@ -5,72 +5,19 @@ import { Navigation } from "./components";
 import { AnimatePresence, motion } from "framer-motion";
 
 const App = () => {
-  const location = useLocation();
-  const [pageVariants, setPageVariants] = useState({
-    hidden: { opacity: 0, y: "-100vh" },
-    visible: { opacity: 1, y: 0, transition: { type: "spring", stiffness: 50 } },
-  });
+  
 
   return (
     <>
       <div className="App">
-        <Navigation />
-        <AnimatePresence exitBeforeEnter={false} mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route
-              path="/"
-              element={
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  variants={pageVariants}
-                >
-                  <HomePage />
-                </motion.div>
-              }
-            ></Route>
-            <Route
-              path="/AboutMe"
-              element={
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  variants={pageVariants}
-                >
-                  <AboutMe />
-                </motion.div>
-              }
-            ></Route>
-            <Route
-              path="/Portfolio"
-              element={
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  variants={pageVariants}
-                >
-                  <Portfolio />
-                </motion.div>
-              }
-            ></Route>
-            <Route
-              path="*"
-              element={
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  exit="hidden"
-                  variants={pageVariants}
-                >
-                  <div>404 Not Found</div>
-                </motion.div>
-              }
-            ></Route>
-          </Routes>
-        </AnimatePresence>
+      <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/AboutMe" element={<AboutMe />}></Route>
+          <Route path="/Portfolio" element={<Portfolio />}></Route>
+          <Route path="*" element={<div>404 Not Found</div>}></Route>
+        </Routes>
+        
       </div>
     </>
   );
